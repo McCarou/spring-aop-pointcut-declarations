f148cd0391ea4339c213b03392bb1436fd32986b
McCarou <alex-mak96@mail.ru>
2021-08-23 12:40:56 +0300

    Added application with pointcut declaration and implementation

---
+++ pointcut/src/com/pointcutexample/dao/AccountDAO.java
@@ -0,0 +1,26 @@
+package com.pointcutexample.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.pointcutexample.Account;
+
+@Component
+public class AccountDAO {
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+		
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		return false;
+	}
+}
+
+
+
+
+
