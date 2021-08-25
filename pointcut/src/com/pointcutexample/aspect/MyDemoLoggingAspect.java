a210795affbbbfed1dc366b7ae8767d84f7ce48f
McCarou <alex-mak96@mail.ru>
2021-08-25 09:47:58 +0300

    Added joinpoint methodSignature and methodParameters

--- pointcut/src/com/pointcutexample/aspect/MyDemoLoggingAspect.java
+++ pointcut/src/com/pointcutexample/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,11 @@
 package com.pointcutexample.aspect;
 
+import com.pointcutexample.Account;
+import org.aspectj.apache.bcel.classfile.MethodParameters;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +15,25 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.pointcutexample.aspect.MyAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n=====>>> Executing @Before advice on method");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        //display the method args
+        Object[] methodParameters = theJoinPoint.getArgs();
+        for (Object tempArg : methodParameters) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
     }
 
 }
